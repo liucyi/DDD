619a9228ecb492d3332dc1f372218fb2743b56e0
liucyi <liucyi@sina.cn>
2015-12-19 16:48:14 +0800

    添加用户全名

--- DDD.Domain/Entity/SysPerson.cs
+++ DDD.Domain/Entity/SysPerson.cs
@@ -12,6 +12,7 @@ namespace DDD.Domain.Entity
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public string FullName { get; set; }
         public string Password { get; set; }
         public string Sex { get; set; }
         public int SysRoleId { get; set; }
